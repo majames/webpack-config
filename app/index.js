064c123de8a29afca8fc54943723df783de376a5
Michael James <mjames@atlassian.com>
2015-11-08 15:53:20 +1100

    Added stage-0 preset to babel loader to allow experimentation with es7 features which have made it to or beyond the strawman stage

--- app/index.js
+++ app/index.js
@@ -6,4 +6,6 @@ import { MyMath } from './utils';
 document.getElementsByTagName('body')[0].innerHTML = `Hi Michael!
   Would you like some ${MyMath.PI}?`;
 
+console.log(`pi cubed is ${MyMath.PI ** 3}`);
+
 debugger;
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['es2015']
+          presets: ['es2015', 'stage-0']
         }
       }
     ]
